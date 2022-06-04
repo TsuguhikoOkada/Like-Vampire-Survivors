2aca26ca8647130b200afbc68b92c5dda0d50d2c
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-04 12:14:53 +0900

    Playerのアニメーターと呼び出し用スクリプトを作成した。

---
+++ Assets/Scripts/Player/PlayerAnimeBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Playerのアニメーションを管理するスクリプト
+/// </summary>
+public class PlayerAnimeBase : MonoBehaviour
+{
+    /// <summary> Playerのアニメーターのメンバ変数</summary>
+    Animator m_animator => GetComponent<Animator>();
+
+    /// <summary> Playerのアニメーター(外部呼び出し用)のメンバ変数</summary>
+    public Animator Animotion =>m_animator;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
