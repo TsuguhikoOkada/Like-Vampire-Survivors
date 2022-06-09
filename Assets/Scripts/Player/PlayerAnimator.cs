12dc50afa4ddf7cca42fe010398eb3a4c35c6f05
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-09 13:28:39 +0900

    PlayerのMortionスクリプトを作成した。

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Playerのアニメーターを管理するスクリプト
+/// </summary>
+public class PlayerAnimator : MonoBehaviour
+{
+    /// <summary> Playerのアニメーターのメンバ変数</summary>
+    Animator m_animator => GetComponent<Animator>();
+
+    /// <summary> Playerのアニメーター(外部呼び出し用)のメンバ変数</summary>
+    public Animator Animotion => m_animator; 
+
+
+    /// <summary>
+    /// Playerのアニメーターのモーション判定のメソッド
+    /// </summary>
+    /// <param name="h_move"></param>
+    /// <param name="v_move"></param>
+    public void JudgeAnimotion (float h_move , float v_move)
+    {
+        m_animator.SetFloat("Direction_X",h_move);
+
+        m_animator.SetFloat("Direction_Y",v_move);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMortion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Playerの移動に関するスクリプト
+/// </summary>
+
+public class PlayerMortion : MonoBehaviour
+{
+    
+}
