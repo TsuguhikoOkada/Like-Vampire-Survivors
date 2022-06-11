e7e0986b30c5f9b5493b25201e14664e7c77e625
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-11 12:58:06 +0900

    PlayerAnimator.csとPlayerMortion.csを編集した。

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -3,24 +3,24 @@
 /// <summary>
 /// Playerのアニメーターを管理するスクリプト
 /// </summary>
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator
 {
     /// <summary> Playerのアニメーターのメンバ変数</summary>
-    Animator m_animator => GetComponent<Animator>();
+    Animator m_animator;
 
     /// <summary> Playerのアニメーター(外部呼び出し用)のメンバ変数</summary>
-    public Animator Animotion => m_animator; 
+    public Animator Animotion { get => m_animator; set => m_animator = value; }
 
 
     /// <summary>
     /// Playerのアニメーターのモーション判定のメソッド
     /// </summary>
-    /// <param name="h_move"></param>
-    /// <param name="v_move"></param>
-    public void JudgeAnimotion (float h_move , float v_move)
+    /// <param name="h_animator_move"></param>
+    /// <param name="">v_animator_move</param>
+    public void JudgeAnimotion (float h_animator_move , float v_animator_move)
     {
-        m_animator.SetFloat("Direction_X",h_move);
+        m_animator.SetFloat("Direction_X",h_animator_move);
 
-        m_animator.SetFloat("Direction_Y",v_move);
+        m_animator.SetFloat("Direction_Y",v_animator_move);
     }
 }
--- Assets/Scripts/Player/PlayerMortion.cs
+++ Assets/Scripts/Player/PlayerMortion.cs
@@ -6,7 +6,22 @@ using UnityEngine;
 /// Playerの移動に関するスクリプト
 /// </summary>
 
-public class PlayerMortion : MonoBehaviour
+public class PlayerMortion 
 {
     
+    public float PlayerSpeed;
+    Rigidbody2D m_playerRigid2D;
+
+    PlayerAnimator playerAnimator;
+
+    
+
+    public void Move(float h_move,float v_move)
+    {
+        Vector2 direction = new Vector2(h_move, v_move).normalized;
+
+        m_playerRigid2D.velocity = direction * PlayerSpeed;
+
+        playerAnimator.JudgeAnimotion(h_move, v_move);
+    }
 }
