c1d4dd2b3089f376c25987a92ff016b7f3574163
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-11 13:08:41 +0900

    動作やシステム作りの参考にするための参考ファイルを作成し、それらのスクリプトを追加。

---
+++ Assets/Scripts/References/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+/// <summary>弾のスクリプト</summary>
+
+public class Bullet : MonoBehaviour
+{
+    /// <summary>弾の速度を表すメンバ変数</summary>
+    [SerializeField, Header("弾速度")] Vector2 _bulletVelocity;
+
+    /// <summary>弾のRigidBody2Dのメンバ変数</summary>
+    Rigidbody2D _rb2DB => GetComponent<Rigidbody2D>();
+
+    IObjectPool<Bullet> _pool;
+
+    public void SetPool(IObjectPool<Bullet> pool)
+    {
+        _pool = pool;
+    }
+
+    /// <summary>
+    /// 弾の速度計算をFixed Updateで実行
+    /// </summary>
+    void FixedUpdate()
+    {
+        _rb2DB.velocity = _bulletVelocity;
+    }
+
+    /// <summary>
+    /// カメラに見えなくなったら自身を消す
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        _pool?.Release(element:this);
+    }
+}
---
+++ Assets/Scripts/References/Launcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+/// <summary>銃のスクリプト</summary>
+public class Launcher : MonoBehaviour
+{
+    [SerializeField, Header("弾のプレハブ")] Bullet _bulletPrefab;
+
+    [SerializeField, Header("1回の弾の最大数")] int _maxBullet;
+
+    IObjectPool<Bullet> _bulletPool;
+    void Awake()
+    {
+        _bulletPool = new ObjectPool<Bullet>(CreateBullet, OnGetBullet
+                                                                , OnReleaseBullet, OnDestroyBullet
+                                                                , maxSize: _maxBullet
+                                                             );
+    }
+
+    Bullet CreateBullet ()
+    {
+        Bullet bullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+
+        bullet.SetPool(_bulletPool);
+
+        return bullet;
+    }
+
+    void OnGetBullet(Bullet bulletObj)
+    {
+        bulletObj.gameObject.SetActive(true);
+
+        bulletObj.transform.position = transform.position;
+    }
+
+    void OnReleaseBullet(Bullet bulletObj)
+    {
+        bulletObj.gameObject.SetActive(false);
+    }
+
+    void OnDestroyBullet(Bullet bulletObj)
+    {
+        Destroy(bulletObj.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _bulletPool?.Get();
+        }
+    }
+}
