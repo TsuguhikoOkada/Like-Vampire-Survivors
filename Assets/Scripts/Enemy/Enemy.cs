1eac792684a5a5db4ceb70c06a9eb7c17061e892
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-13 14:04:34 +0900

    EnemyMortion.csを追加し、PlayerMortion.csと同じようにコーディングした。

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField,Header("敵のデータ")] EnemyData enemyData;
 
-    [SerializeField, Header("敵の移動速度")] float _enemySpeed;
+    
     void Start()
     {
         
---
+++ Assets/Scripts/Enemy/EnemyMortion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Enemyの動作に関するスクリプト</summary>
+public class EnemyMortion
+{
+    Rigidbody2D m_enemyRigid2D;
+
+    public Rigidbody2D EnemyRigid2D { get => m_enemyRigid2D; set => m_enemyRigid2D = value; }
+
+
+    public void EnemyMove(float enemy_h_move,float enemy_v_move)
+    {
+        Vector2 enemyDirection = new Vector2(enemy_h_move,enemy_v_move).normalized;
+
+        m_enemyRigid2D.velocity = enemyDirection;
+    }
+}
