758d0c453d5ff75b660b9c5cb9967c7b909f9b57
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-13 13:27:59 +0900

    GeneratorTestのスクリプトとEnemyのスクリプトを追加した。

---
+++ Assets/Scripts/GeneratorTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>敵を出現させるためのスクリプト(試作中)</summary>
+public class GeneratorTest : MonoBehaviour
+{
+	public string targetObjectName; // 目標オブジェクト名：Inspectorで指定
+	public float speed = 1; // スピード：Inspectorで指定
+
+	GameObject targetObject;
+	Rigidbody2D rbody;
+
+	void Start()
+	{ // 最初に行う
+	  // 目標オブジェクトを見つけておく
+		targetObject = GameObject.Find(targetObjectName);
+		// 重力を0にして、衝突時に回転させない
+		rbody = GetComponent<Rigidbody2D>();
+		rbody.gravityScale = 0;
+		rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+
+	void FixedUpdate()
+	{ // ずっと行う（一定時間ごとに）
+	  // 目標オブジェクトの方向を調べて
+		Vector3 dir = (targetObject.transform.position - this.transform.position).normalized;
+		// その方向へ指定した量で進む
+		float vx = dir.x * speed;
+		float vy = dir.y * speed;
+		rbody.velocity = new Vector2(vx, vy);
+		// 移動の向きで左右に向きを変える
+		this.GetComponent<SpriteRenderer>().flipX = (vx < 0);
+	}
+}
