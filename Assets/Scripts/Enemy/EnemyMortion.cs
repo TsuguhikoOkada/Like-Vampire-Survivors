8293a56f8d94001a1fe3cf184931e6ccc239ab45
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-14 08:19:56 +0900

    EnemyMortion.csにplayer追随用のTransformを追加した。

--- Assets/Scripts/Enemy/EnemyMortion.cs
+++ Assets/Scripts/Enemy/EnemyMortion.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 /// <summary>Enemyの動作に関するスクリプト</summary>
+[Serializable]
 public class EnemyMortion
 {
     Rigidbody2D m_enemyRigid2D;
 
     public Rigidbody2D EnemyRigid2D { get => m_enemyRigid2D; set => m_enemyRigid2D = value; }
 
-
+    [SerializeField]Transform m_target_Transform;
     public void PlayerChase(TargetEnum targetChase,float enemy_h_move,float enemy_v_move)
     {
         //targetChase = TargetEnum.Player;
